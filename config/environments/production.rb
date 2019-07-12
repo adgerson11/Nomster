96d8c5c3511a6dcba503cfd6ad7a4a5fa1d81e4b
Shamark Adgerson <adgerson11@gmail.com>
2019-07-12 18:53:39 +0000

    Setting up Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  configure.action_mailer.default_url_options = { host: 'nomster-shamark-adgerson.herokuapp.com' }
+
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
