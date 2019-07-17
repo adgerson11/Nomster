53673f82a59c2f97a3ca796c18cf2b49343c5413
Shamark Adgerson <adgerson11@gmail.com>
2019-07-17 19:52:50 +0000

    Updating the place data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,8 +19,15 @@ class PlacesController < ApplicationController
     end
 
     def edit
-        @place =Place.find(params[:id])
+        @place = Place.find(params[:id])
     end
+
+    def update
+        @place = Place.find(params[:id])
+        @place.update_attributes(place_params)
+        redirect_to root_path
+    end
+
 
     private
 
