9b0f72de8dd348bba389de9001f5e0e1549d4067
Shamark Adgerson <adgerson11@gmail.com>
2019-08-02 16:49:21 +0000

    Error with saving files to database

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,14 +3,13 @@ class PhotosController < ApplicationController
 
     def create
         @place =Place.find(params[:place_id])
-        @place.photos.create(photo_params(user: current_user))
-        render json: {picture: photo_params}
+        @place.photos.create(photo_params.merge(user: current_user))
         redirect_to place_path(@place)
     end
 
     private
 
     def photo_params
-        photo_params.require(:picture).permit(:caption)
+        params.require(:photo).permit(:captions, :picture)
     end
 end
